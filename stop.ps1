230ac92d02fd182dd3e0b2c087288ced47ea512c
cayvonmorady <13281360+cayvonmorady@users.noreply.github.com>
2024-12-12 18:56:58 -0800

    Restart

--- stop.ps1
+++
@@ -1,8 +0,0 @@
-# Stop.ps1
-Write-Output "Stopping the Docker container..."
-docker stop my-website-container
-
-Write-Output "Removing the Docker container..."
-docker rm my-website-container
-
-Write-Output "Container stopped and removed."
