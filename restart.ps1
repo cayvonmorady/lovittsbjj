1661c3ba55c7e6c1a484f979679ab8284bf406fe
cayvon-papercrow <cayvon.morady@gmail.com>
2024-12-12 18:56:58 -0800

    Restart

--- restart.ps1
+++
@@ -1,4 +0,0 @@
-# Restart.ps1
-Write-Output "Restarting the Docker container..."
-.\stop.ps1
-.\start.ps1
--- start.ps1
+++
@@ -1,8 +0,0 @@
-# Start.ps1
-Write-Output "Building the Docker image..."
-docker build -t my-website .
-
-Write-Output "Starting the Docker container..."
-docker run -d -p 8080:80 --name my-website-container my-website
-
-Write-Output "Website is running at http://localhost:8080"
