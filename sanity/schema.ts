b067cde2c05c60765baf190461dbeee7591eb574
cayvon-papercrow <cayvon.morady@gmail.com>
2025-01-29 11:38:17 -0800

    Delete non-needed schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,9 @@
 import homepage from './schemas/homepage'
 import pricing from './schemas/pricing'
-import gallery from './schemas/gallery'
 import instructor from './schemas/instructor'
-import schedule from './schemas/schedule'
 import galleryImage from './schemas/galleryImage'
 import classSchema from './schemas/class'
 
 export const schema = {
-  types: [homepage, pricing, gallery, instructor, schedule, galleryImage, classSchema],
+  types: [homepage, pricing, instructor, galleryImage, classSchema],
 }
