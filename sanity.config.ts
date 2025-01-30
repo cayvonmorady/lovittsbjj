cf32ca7b46ab589664cf6e13b6bf0e14e18c3daf
cayvonmorady <13281360+cayvonmorady@users.noreply.github.com>
2025-01-29 17:43:04 -0800

    Sanity Dashboard

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,37 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { schema } from './sanity/schema'
+import { structure } from './sanity/desk/structure'
+import { dashboardTool } from '@sanity/dashboard'
+import { documentListWidget } from 'sanity-plugin-dashboard-widget-document-list'
 
 export default defineConfig({
   name: 'default',
   title: 'Lovitts BJJ',
   projectId: 'xtgasnb2',
   dataset: 'production',
-  plugins: [deskTool()],
+  plugins: [
+    deskTool({
+      structure
+    }),
+    dashboardTool({
+      widgets: [
+        documentListWidget({
+          title: 'Last edited documents',
+          order: '_updatedAt desc',
+          limit: 10,
+          types: ['pricing', 'schedule', 'instructor', 'gallery', 'homepage'],
+          layout: { width: 'medium' }
+        }),
+        documentListWidget({
+          title: 'Unpublished changes',
+          query: '*[_id in path("drafts.**")]',
+          limit: 10,
+          layout: { width: 'medium' }
+        })
+      ]
+    })
+  ],
   schema: schema,
   basePath: '/studio',
 })
