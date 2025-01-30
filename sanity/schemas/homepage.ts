2030a0291e99f03a39d12803c2a7e920519faa29
cayvonmorady <13281360+cayvonmorady@users.noreply.github.com>
2025-01-29 16:27:08 -0800

    Remove chatbot from Studio; populate Sanity

--- sanity/schemas/homepage.ts
+++ sanity/schemas/homepage.ts
@@ -29,6 +29,36 @@ export default {
       ]
     },
     {
+      name: 'alert',
+      title: 'Alert Banner',
+      type: 'object',
+      fields: [
+        {
+          name: 'isActive',
+          title: 'Show Alert',
+          type: 'boolean',
+          initialValue: false
+        },
+        {
+          name: 'message',
+          title: 'Alert Message',
+          type: 'text'
+        },
+        {
+          name: 'type',
+          title: 'Alert Type',
+          type: 'string',
+          options: {
+            list: [
+              { title: 'Info', value: 'info' },
+              { title: 'Warning', value: 'warning' },
+              { title: 'Success', value: 'success' }
+            ]
+          }
+        }
+      ]
+    },
+    {
       name: 'programs',
       title: 'Programs',
       type: 'array',
@@ -55,6 +85,48 @@ export default {
           }
         ]
       }]
+    },
+    {
+      name: 'location',
+      title: 'Location Information',
+      type: 'object',
+      fields: [
+        {
+          name: 'address',
+          title: 'Street Address',
+          type: 'string'
+        },
+        {
+          name: 'city',
+          title: 'City',
+          type: 'string'
+        },
+        {
+          name: 'state',
+          title: 'State',
+          type: 'string'
+        },
+        {
+          name: 'zipCode',
+          title: 'ZIP Code',
+          type: 'string'
+        },
+        {
+          name: 'phone',
+          title: 'Phone Number',
+          type: 'string'
+        },
+        {
+          name: 'email',
+          title: 'Email Address',
+          type: 'string'
+        },
+        {
+          name: 'googleMapsUrl',
+          title: 'Google Maps Link',
+          type: 'url'
+        }
+      ]
     }
   ]
 }
