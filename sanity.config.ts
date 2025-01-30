2d947d7b37b03faaee71e0f9c806f9a74af38d66
cayvonmorady <13281360+cayvonmorady@users.noreply.github.com>
2025-01-29 18:22:56 -0800

    Revert "Sanity Dashboard"
    This reverts commit 7ec7a61139d7d36b22a2c133b997ca08fca3347b.

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,37 +1,13 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { schema } from './sanity/schema'
-import { structure } from './sanity/desk/structure'
-import { dashboardTool } from '@sanity/dashboard'
-import { documentListWidget } from 'sanity-plugin-dashboard-widget-document-list'
 
 export default defineConfig({
   name: 'default',
   title: 'Lovitts BJJ',
   projectId: 'xtgasnb2',
   dataset: 'production',
-  plugins: [
-    deskTool({
-      structure
-    }),
-    dashboardTool({
-      widgets: [
-        documentListWidget({
-          title: 'Last edited documents',
-          order: '_updatedAt desc',
-          limit: 10,
-          types: ['pricing', 'schedule', 'instructor', 'gallery', 'homepage'],
-          layout: { width: 'medium' }
-        }),
-        documentListWidget({
-          title: 'Unpublished changes',
-          query: '*[_id in path("drafts.**")]',
-          limit: 10,
-          layout: { width: 'medium' }
-        })
-      ]
-    })
-  ],
+  plugins: [deskTool()],
   schema: schema,
   basePath: '/studio',
 })
