3b0b3d17b9003054ff4f3bb38604528c4110676f
cayvon-papercrow <cayvon.morady@gmail.com>
2025-03-10 13:20:11 -0700

    Update sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { schema } from './sanity/schema'
+import publishAllTool from './sanity/desk/publishAllTool'
 
 export default defineConfig({
   name: 'default',
   title: 'Lovitts BJJ',
   projectId: 'xtgasnb2',
   dataset: 'production',
-  plugins: [deskTool()],
+  plugins: [deskTool(), publishAllTool],
   schema: schema,
   basePath: '/studio',
 })
