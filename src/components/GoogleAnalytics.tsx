6dd61c00913566c5f1ffb783abac4174397670da
cayvon-papercrow <cayvon.morady@gmail.com>
2025-03-10 12:26:44 -0700

    updates

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-G0HNV5GSPB"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G0HNV5GSPB');
+        `}
+      </Script>
+    </>
+  );
+}
